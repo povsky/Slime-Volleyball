c4f36420e98eeed04972f51a331ab52bb06eb082
povsky <povsky@gmail.com>
2017-09-03 05:09:24 -0400

    Slime bouncing working and eyes
    Bouncing at arbitrary angle working.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@ var canvas, canvasContext;
 
 const BALL_RADIUS = 12;
 const ACCEL_X = 0; 
-const ACCEL_Y = 0.5;//0.38;
+const ACCEL_Y = 0.12;//0.38;
 //const DAMPING = 0.97;
 //const TRACTION = 0.95;
-const DAMPING = 1;
-const TRACTION = 1;
+const DAMPING = 0.97;
+const TRACTION = 0.95;
 const FLOOR_THICKNESS = 70;
+
+const HITBOX_RADIUS = SLIME_RADIUS + BALL_RADIUS;
 
 var ballX = 100;
 var ballY = 200;
-var ballSpeedX = 0;
-var ballSpeedY = -1;
+var ballSpeedX = 1;
+var ballSpeedY = 0;
 
 var bottomLimit;
 var leftLimit;
@@ -21,6 +23,7 @@ var rightLimit;
 var upperLimit;
 
 var redSlime = new slimeClass;
+var whiteSlime = new slimeClass;
 
 	
 window.onload = function() {
@@ -42,8 +45,13 @@ function startGame() {
 	
 	setAreaLimits();
 	redSlime.reset("Red Slime","red");
+	whiteSlime.reset("White Slime","white");
 	
-	var framesPerSecond = 60;
+	whiteSlime.x = 500;
+	redSlime.speedY = -5;
+	whiteSlime.speedY = -5;
+	
+	var framesPerSecond = 120;
 	setInterval(updateAll,1000/framesPerSecond);
 	
 	setupInput();
@@ -92,9 +100,98 @@ function ballPositionIterate() {
 }
 
 function ballVelocityIterate() {
+	
 
 	ballSpeedX += ACCEL_X;
 	ballSpeedY += ACCEL_Y;
+}
+
+function calcSlimeAngle(whichSlime) {
+	
+	var xDiff = (whichSlime.x - ballX);
+	var yDiff = (whichSlime.y - ballY);
+	
+	var angle = -Math.atan2(yDiff,-xDiff);
+	return angle;
+}
+
+function calcCollisionPointSlime(whichSlime) {
+		
+
+		var angle = calcSlimeAngle(whichSlime);
+		console.log(angle*180/Math.PI);
+		
+		var prevBallX = ballX - ballSpeedX;
+		var prevBallY = ballY - ballSpeedY;
+		var intBallX = ballX;
+		var intBallY = ballY;
+		
+		ballX = HITBOX_RADIUS*Math.cos(angle) + whichSlime.x;
+		ballY = HITBOX_RADIUS*Math.sin(angle) + whichSlime.y;
+		
+		var ratioX = (ballX - prevBallX)/(intBallX - prevBallX + 0.00001);
+		var ratioY = (ballY - prevBallY)/(intBallY - prevBallY + 0.00001);
+		
+		console.log(ratioX);
+		
+		ballSpeedX += ratioX*ACCEL_X;
+		ballSpeedY += ratioY*ACCEL_Y;
+		
+		//console.log(ballX+","+ballY);
+		
+}
+
+function calcSlimeBounce(whichSlime) {
+	
+	var N = new Array(2);
+	var V = new Array(2);
+	var R = new Array(2);
+	
+	var angle = calcSlimeAngle(whichSlime);
+	
+	N[0] = Math.cos(angle);
+	N[1] = Math.sin(angle);
+	V[0] = ballSpeedX;
+	V[1] = ballSpeedY;
+	
+	console.log(V)
+	
+	var vDotN = dot(V[0],V[1],N[0],N[1]);
+	
+	R[0] = TRACTION*(-2*vDotN*N[0]) + V[0];// + whichSlime.speedX;
+	R[1] = DAMPING*(-2*vDotN*N[1]) + V[1];// + whichSlime.speedY;
+	
+	ballSpeedX = R[0];
+	ballSpeedY = R[1];
+	
+	if(Math.abs(ballSpeedX) > 20) {
+		ballSpeedX = Math.sign(ballSpeedX)*20;
+	}
+	if(Math.abs(ballSpeedY) > 20) {
+		ballSpeedY = Math.sign(ballSpeedY)*20;
+	}
+	
+	//console.log(N);
+	
+	
+	
+}
+
+function ballSlimeHandling(whichSlime) {
+
+	var diag = ((whichSlime.x - ballX)*(whichSlime.x - ballX)) +
+				((whichSlime.y - ballY)*(whichSlime.y - ballY))
+	
+	if(diag <= HITBOX_RADIUS*HITBOX_RADIUS && ballY < whichSlime.y) {
+		
+		calcCollisionPointSlime(whichSlime);
+		calcSlimeBounce(whichSlime);
+		
+		
+		
+	}
+	
+	
 }
 
 function ballWallHandling() {
@@ -103,14 +200,9 @@ function ballWallHandling() {
 		
 		var prevBallY = ballY - ballSpeedY;
 		var prevBallX = ballX - ballSpeedX;
-		//var ratio = calcRatio(bottomLimit, prevBallY, ballY);
-		//calcBallBounce(ratio, prevBallX, prevBallY, TRACTION, -1*DAMPING);		
-
-		
-		
-		
-		
-		
+		var ratio = calcRatio(bottomLimit, prevBallY, ballY);
+		calcBallBounce(ratio, prevBallX, prevBallY, TRACTION, -1*DAMPING);		
+	
 	}else if(ballY <= upperLimit) {
 		
 		var prevBallY = ballY - ballSpeedY;
@@ -140,7 +232,12 @@ function ballWallHandling() {
 function moveBall() {
 	
 	ballPositionIterate();
+	
 	ballWallHandling();
+	
+	ballSlimeHandling(redSlime);
+	ballSlimeHandling(whiteSlime);
+	
 	ballVelocityIterate();
 	
 }
@@ -149,6 +246,7 @@ function moveAll() {
 	
 	moveBall();
 	redSlime.move();
+	whiteSlime.move();
 	
 }
 
@@ -161,6 +259,7 @@ function drawAll() {
 	
 	drawBall();
 	redSlime.draw();
+	whiteSlime.draw();
 	
 }
 
--- js/slime.js
+++ js/slime.js
@@ -2,13 +2,13 @@
 const SLIME_RADIUS = 40;
 const SLIME_SPEED = 1;
 const JUMP_HEIGHT = 100;
-const SLIME_ACCEL_Y = 0.5;
-const SLIME_START_X = 100;
+const SLIME_ACCEL_Y = 0.12;
+const SLIME_START_X = 200;
 const SLIME_SPEED_X = 5;
 const SLIME_SPEED_Y = 10;
 
-const SLIME_EYE_RADIUS = 5;
-const SLIME_PUPIL_RADIUS = 2;
+const SLIME_EYE_RADIUS = 6;
+const SLIME_PUPIL_RADIUS = 4;
 
 function slimeClass() {
 	
@@ -39,7 +39,7 @@ function slimeClass() {
 		
 		this.x = SLIME_START_X;
 		this.y = canvas.height - FLOOR_THICKNESS;
-		this.speedX = 1;
+		this.speedX = 0;
 		this.speedY = 0;
 		this.name = slimeName;
 		this.color = color;
@@ -70,7 +70,7 @@ function slimeClass() {
 		this.y += this.speedY;		
 		this.speedY += SLIME_ACCEL_Y;
 		
-		if(this.y > canvas.height - FLOOR_THICKNESS - 0.1){	
+		if(this.y > canvas.height - FLOOR_THICKNESS){	
 			this.y = canvas.height - FLOOR_THICKNESS;
 			this.speedY = 0;	
 		}
@@ -84,6 +84,26 @@ function slimeClass() {
 		canvasContext.beginPath();
 		canvasContext.arc(this.x,this.y,SLIME_RADIUS,0,Math.PI,true);
 		canvasContext.fill();
+		
+		var eyeX = SLIME_RADIUS*0.6;
+		var angle = calcSlimeAngle(this);
+		var eyeOffset = SLIME_EYE_RADIUS - SLIME_PUPIL_RADIUS;
+		var centerX = Math.cos(angle)*eyeOffset;
+		var centerY = Math.sin(angle)*eyeOffset;
+		
+		
+		if(this.x <= canvas.width/2) {
+			eyeX *= 1;
+		}else {
+			eyeX *= -1;
+		}
+		
+		colorCircle(this.x + eyeX,this.y - SLIME_RADIUS*0.3,SLIME_EYE_RADIUS,"white");
+		
+		colorCircle(this.x + eyeX + centerX, this.y - SLIME_RADIUS*0.3 + centerY,
+						SLIME_PUPIL_RADIUS, "black");
+		
+		
 		
 	}
 	
